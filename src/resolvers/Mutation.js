a5d3760a192cc3fbcf3a2647f7b959e0e3ad62f8
nfmontenegro <nicolas.fmontenegro@gmail.com>
2019-02-24 23:17:23 -0300

    new mutation createUser

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,21 @@
 import {forwardTo} from 'prisma-binding'
 
 const Mutations = {
-  createUser: forwardTo('db')
+  //validate if user exist
+  async createUser(parent, {data}, ctx, info) {
+    const {name} = data
+    const user = await ctx.db.query.users({where: {name}})
+    if (user.length > 0) throw new Error('User exist!')
+
+    return ctx.db.mutation.createUser(
+      {
+        data: {
+          name
+        }
+      },
+      info
+    )
+  }
 }
 
 export default Mutations

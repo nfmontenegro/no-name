913b2929a6a1b71bb9495cfbc2e8f12f33c9554d
Nicolás <nicolas@tactech.cl>
2019-03-06 22:01:52 -0300

    first steps to test

--- src/test/resolver.test.js
+++ src/test/resolver.test.js
@@ -1,15 +1,10 @@
 import {createApolloFetch} from 'apollo-fetch'
 
-const {HOST, PORT} = process.env
 test('adds 1 + 2 to equal 3', () => {
   const apolloFetch = createApolloFetch({
-    uri: `${HOST}${PORT}`,
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjanN0a29nZzFib3hqMGI4NzJpZnRmY2l3IiwiaWF0IjoxNTUxNjU5MTMxfQ.4F7iRY1uQ67xNKAGHAl6HX3HVom0HuZhIvHxnIir_ig'
-    }
+    uri: `http://localhost:4000/`
   })
-  console.log(HOST, PORT)
+
   apolloFetch({
     query: `mutation login($email: String!, $password: String!) {
   login(data: {email: $email, password: $password}) {
@@ -24,5 +19,5 @@ test('adds 1 + 2 to equal 3', () => {
 }
 `,
     variables: {email: 'new2@gmail.com', password: '123'}
-  }).then(response => expect(response).toBe(true))
+  }).then(response => expect(response).toBeTruthy())
 })

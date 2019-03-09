7d0f02c8f835900a1d01e884abeade4a49be2018
nfmontenegro <nicolas.fmontenegro@gmail.com>
2019-03-08 23:13:49 -0300

    check env variables

--- src/test/client.js
+++ src/test/client.js
@@ -4,9 +4,8 @@ require('dotenv').config()
 
 const prisma = new Prisma({
   typeDefs: __dirname + '/prisma.graphql',
-  endpoint: 'https://eu1.prisma.sh/nicolas-flores-montenegro-f8a898/now-prisma/dev',
-  secret:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJub3ctcHJpc21hQGRldiIsInJvbGVzIjpbImFkbWluIl19LCJpYXQiOjE1NTIwOTM4NDcsImV4cCI6MTU1MjY5ODY0N30.PlfgmfjrCIlQgGd4dANl7Zw-kvzAr4h97MlLtCqfJpw'
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_PASSWORD
 })
 
 module.exports = prisma

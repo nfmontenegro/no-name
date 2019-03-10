76436b895517e1b6bb6c72a4b62eff9231de2e09
nfmontenegro <nicolas.fmontenegro@gmail.com>
2019-03-10 19:32:44 -0300

    test mockups

--- src/test/client.js
+++ src/test/client.js
@@ -1,9 +1,11 @@
 const {Prisma} = require('prisma-binding')
+const fs = require('fs')
+const path = require('path')
 
 require('dotenv').config()
 
 const prisma = new Prisma({
-  typeDefs: __dirname + '/prisma.graphql',
+  typeDefs: path.join(__dirname, '../generated/prisma.graphql'),
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_PASSWORD
 })

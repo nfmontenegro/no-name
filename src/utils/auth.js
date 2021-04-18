c4e2f797c60b90a890cd7b1ec90884ddcbe28f73
Nicolás Flores Montenegro <nicolas.fmontenegro@gmail.com>
2021-04-18 00:56:38 -0400

    fix: remove unused variable

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {useEffect, useState} from 'react'
 import {StatusCodes} from 'http-status-codes'
 import {Redirect} from 'react-router-dom'
